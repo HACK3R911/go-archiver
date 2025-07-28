4f377adf4f5b5fd108ce6b21df202eddc0af728d
HACK3R911 <afanasiev-2004@list.ru>
2025-07-28 12:32:26 +0300

    uncommit execute

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 package main
 
+import "github.com/HACK3R911/go-archiver/cmd"
+
+// archiver pack <arch_mode> </path/to/file> -out </path/to/packed-file>
 func main() {
-	// cmd.Execute()
+	cmd.Execute()
 }
